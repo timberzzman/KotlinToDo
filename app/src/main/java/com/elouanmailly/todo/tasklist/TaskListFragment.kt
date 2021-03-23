32b20a8dfb59a25b60881e9b63dba626a49b13bd
Elouan MAILLY <leloupandco@gmail.com>
2021-03-23 22:46:17 +0100

    Using userInfoViewModel in fragment

--- app/src/main/java/com/elouanmailly/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/elouanmailly/todo/tasklist/TaskListFragment.kt
@@ -12,11 +12,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.elouanmailly.todo.databinding.FragmentTaskListBinding
-import com.elouanmailly.todo.network.Api
 import com.elouanmailly.todo.task.TaskActivity
 import kotlinx.coroutines.launch
 import com.elouanmailly.todo.network.TaskListViewModel
+import com.elouanmailly.todo.network.UserInfoViewModel
+import com.elouanmailly.todo.userinfo.UserInfoActivity
 
 class TaskListFragment : Fragment() {
     override fun onCreateView(
@@ -35,12 +38,22 @@ class TaskListFragment : Fragment() {
         viewModel.taskList.observe(viewLifecycleOwner) { list ->
             adapter.submitList(list.toList())
         }
+        userViewModel.userinfo.observe(viewLifecycleOwner) {
+            viewBinding.fragmentTaskListTitle.text = "${it.firstName} ${it.lastName}"
+            viewBinding.fragmentTaskListImage.load(it.avatar) {
+                transformations(CircleCropTransformation())
+            }
+        }
         viewBinding.addTaskButton.setOnClickListener {
             val intent = Intent(activity, TaskActivity::class.java)
             startForResult.launch(intent)
         }
+        viewBinding.listHeader.setOnClickListener {
+            val intent = Intent(activity, UserInfoActivity::class.java)
+            startForResult.launch(intent)
+        }
         adapter.onDeleteTask = { task ->
-                lifecycleScope.launch {
+            lifecycleScope.launch {
                 viewModel.deleteTask(task)
             }
         }
@@ -51,30 +64,29 @@ class TaskListFragment : Fragment() {
         }
     }
 
-    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-        if (result.resultCode == RESULT_OK) {
-            val task = result.data?.getSerializableExtra(TaskActivity.TASK_KEY) as? Task
-            if (task != null) {
-                val position = viewModel.taskList.value?.indexOfFirst { it.id == task.id }
-                if (position  == -1) {
-                    viewModel.addTask(task)
-                } else {
-                    viewModel.editTask(task)
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                val task = result.data?.getSerializableExtra(TaskActivity.TASK_KEY) as? Task
+                if (task != null) {
+                    val position = viewModel.taskList.value?.indexOfFirst { it.id == task.id }
+                    if (position == -1) {
+                        viewModel.addTask(task)
+                    } else {
+                        viewModel.editTask(task)
+                    }
                 }
             }
         }
-    }
 
     override fun onResume() {
         super.onResume()
-        lifecycleScope.launch {
-            val userInfo = Api.userService.getInfo().body()!!
-            viewBinding.fragmentTaskListTitle.text = "${userInfo.firstName} ${userInfo.lastName}"
-            viewModel.loadTasks()
-        }
+        viewModel.loadTasks()
+        userViewModel.loadInfo()
     }
 
     private lateinit var viewBinding: FragmentTaskListBinding
-    private var adapter : TaskListAdapter = TaskListAdapter()
+    private var adapter: TaskListAdapter = TaskListAdapter()
     private val viewModel: TaskListViewModel by viewModels()
+    private val userViewModel: UserInfoViewModel by viewModels()
 }
